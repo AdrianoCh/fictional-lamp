a07f6f0b54d6f9e62c43e9608eb2ee5de8a183f0
Cairo <carlos.fortes@outlook.com>
2018-06-26 02:16:52 -0300

    Motorista Activiy com Recycler View (Não funcional)

--- sigavan/app/src/main/java/fadergs/edu/br/sigavan/MotoristaActivity.java
+++ sigavan/app/src/main/java/fadergs/edu/br/sigavan/MotoristaActivity.java
@@ -4,11 +4,22 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+import org.w3c.dom.Text;
 
 public class MotoristaActivity extends AppCompatActivity {
     private FloatingActionButton floatingActionButton;
     private  FloatingActionButton floatingActionButton2;
+    private RecyclerView recyclerView;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +44,39 @@ public class MotoristaActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.rvPassageiros);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("/users");
+        FirebaseRecyclerAdapter<PerfilUsuarioPassageiro, pupviewholder> recyclerAdapter=new FirebaseRecyclerAdapter<PerfilUsuarioPassageiro,pupviewholder> (
+                PerfilUsuarioPassageiro.class,
+                R.layout.viewholder,
+                mDatabaseReference) {
+            @Override
+            protected void populateViewHolder (pupviewholder viewholder, PerfilUsuarioPassageiro pup, int position) {
+                viewholder.setNome(pup.getNome());
+                // viewholder.setPresenca();
+            }
+        };
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+    public static class pupviewholder extends RecyclerView.ViewHolder {
+        View mView;
+        TextView textView_nome;
+        TextView textView_presenca;
+        public pupviewholder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            textView_nome = (TextView) itemView.findViewById(R.id.nomepassageiro);
+            textView_presenca = (TextView) itemView.findViewById(R.id.presencapassageiro);
+
+        }
+        public void setNome (String nome) {
+            textView_nome.setText(nome);
+        }
+        public void setPresenca (String presenca) {
+            textView_nome.setText(presenca);
+        }
     }
 }
